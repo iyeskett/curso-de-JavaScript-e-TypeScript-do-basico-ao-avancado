7182c923ac46b7fd7e87d46fb526c21f75e954be
iyeskett <felipesilva.mello27@gmail.com>
2023-03-22 20:50:48 -0300

    Aula04:ComentariosDeCódigo

---
+++ aula04/comentarios.js
@@ -0,0 +1,13 @@
+// Comentário
+console.log(`Olá, Mundo!`);
+console.log(`Linha 1`);
+
+/* 
+
+    console.log(`Linha 2`);
+    console.log(`Linha 3`);
+    console.log(`Linha54`);
+    
+*/
+
+console.log(`Linha5454`);
