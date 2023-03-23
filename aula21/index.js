1c0576c271dd67e1fa4c251c1342be36f71e9a44
iyeskett <felipesilva.mello27@gmail.com>
2023-03-22 21:05:18 -0300

    Aula21:OperadoresDeComparacao

---
+++ aula21/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+> maior ue
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+
+const comp = 5 >= 5;
+
+console.log(comp);
