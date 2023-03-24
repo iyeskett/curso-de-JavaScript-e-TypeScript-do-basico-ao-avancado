c2f525561328da314d28d06f6d4b0eb8a2c24983
iyeskett <felipesilva.mello27@gmail.com>
2023-03-23 23:31:43 -0300

    Aula62:RevisaoDoBasicoEmArrays

---
+++ aula62/index.js
@@ -0,0 +1,29 @@
+// // Valor por referência
+// const nomes = ['Felipe', 'Joao', 'Maria', 'Teresa'];
+// const novo = [...nomes];
+// const novo2 = nomes.slice(0, 3);
+
+// // nomes[2] = 'Teresa';
+
+// // delete nomes[2];
+// nomes.shift();
+// novo.pop();
+// nomes.push('Felipe');
+// nomes.unshift('Teste');
+
+
+
+// console.log(nomes);
+// console.log(nomes.length)
+// // console.log(novo);
+// console.log(novo2)
+
+
+const nome = 'Felipe Silva Mello';
+const nomes = nome.split(' ');
+
+const nomeJoin = nomes.join(' ');
+
+console.log(nomes);
+console.log(nomeJoin);
+
