1f989ac4aee51f4656e0476e3776ee65f8614536
iyeskett <felipesilva.mello27@gmail.com>
2023-03-30 22:12:37 -0300

    Aula89:XMLHttpRequest(GET)+Promises

---
+++ aula89/assets/js/main.js
@@ -0,0 +1,48 @@
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send(null);
+
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        });
+    })
+
+
+}
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregarPagina(el);
+    }
+});
+
+async function carregarPagina(el) {
+    try {
+        const href = el.getAttribute('href');
+        const objConfig = {
+            method: 'GET',
+            url: href
+        };
+
+        const response = await request(objConfig);
+        carregarResultado(response);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+function carregarResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
