0667502c48650d039cec58cdb700f7ca15b634fd
iyeskett <felipesilva.mello27@gmail.com>
2023-04-05 20:39:12 -0300

    Aula05:EscrevendoELendoArquivos

---
+++ node/aula05/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, 'teste.json');
+const escrever = require('./modules/escrever');
+const ler = require('./modules/ler');
+
+const pessoas = [
+    { nome: 'João' },
+    { nome: 'Maria' },
+    { nome: 'Eduardo' },
+    { nome: 'Luiza' }
+];
+
+const json = JSON.stringify(pessoas, '', 2);
+escrever(caminhoArquivo, json);
+
+async function lerArquivo(caminho) {
+    const dados = await ler(caminho);
+    renderizarDados(dados);
+}
+
+function renderizarDados(dados) {
+    dados = JSON.parse(dados);
+    dados.forEach(val => console.log(val));
+}
+
+lerArquivo(caminhoArquivo);
