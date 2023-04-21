e3bb54e1a39ee44b72712ea17800cfe6df79b1e3
iyeskett <felipesilva.mello27@gmail.com>
2023-04-21 01:21:13 -0300

    api_rest:RotasDeAlunos

--- api_rest/src/controller/HomeController.js
+++ api_rest/src/controller/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Felipe',
-      sobrenome: 'Mello',
-      email: 'felipe.mello@email.com',
-      idade: 400,
-      peso: 600,
-      altura: 3.5,
-    });
-    res.json(novoAluno);
+    res.json('Ok');
   }
 }
 
