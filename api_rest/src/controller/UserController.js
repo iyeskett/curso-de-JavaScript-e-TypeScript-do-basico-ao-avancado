708e55c2e7d41b597e8484765f94a40423bf040e
iyeskett <felipesilva.mello27@gmail.com>
2023-04-20 23:56:23 -0300

    api_rest:MiddlewareDeVerificacaoDoJWT

--- api_rest/src/controller/UserController.js
+++ api_rest/src/controller/UserController.js
@@ -15,6 +15,7 @@ class UserController {
   // Index
   async index(req, res) {
     try {
+      console.log(req.userEmail, req.userId);
       const users = await User.findAll();
       return res.json(users);
     } catch (e) {
--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userController from '../controller/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+router.get('/', loginRequired, userController.index);
 router.post('/', userController.create);
-router.get('/', userController.index);
 router.get('/:id', userController.show);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
