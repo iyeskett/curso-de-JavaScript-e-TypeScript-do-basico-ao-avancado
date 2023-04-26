c644acfd554aae9fb0f704505e704f98babc6d17
iyeskett <felipesilva.mello27@gmail.com>
2023-04-25 22:05:33 -0300

    api_rest:CRUDDeAlunos

--- api_rest/src/controller/AlunoController.js
+++ api_rest/src/controller/AlunoController.js
@@ -5,6 +5,100 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async create(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+
+      const novoAluno = await aluno.update(req.body);
+
+      return res.json(novoAluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
