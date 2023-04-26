40d1f4c9e345f5b822c85c4bf5ca608093e0eac7
iyeskett <felipesilva.mello27@gmail.com>
2023-04-26 18:45:52 -0300

    api_rest:TratandoOsArquivosEnviados

--- api_rest/src/controller/FotoController.js
+++ api_rest/src/controller/FotoController.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async create(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- api_rest/src/routes/fotoRoutes.js
+++ api_rest/src/routes/fotoRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controller/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.create);
+router.post('/', fotoController.create);
 
 export default router;
