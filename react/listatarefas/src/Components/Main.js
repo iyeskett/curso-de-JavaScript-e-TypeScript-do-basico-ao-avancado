2c2738d11e2d3ea30ef05bd0cbea8dc9930d12a5
Felipe Mello <felipesilva.mello27@gmail.com>
2023-05-04 22:20:51 -0300

    react:EstilizandoOForm

--- react/listatarefas/src/Components/Main.js
+++ react/listatarefas/src/Components/Main.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+
+// Form
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FaPlus } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
@@ -13,16 +18,17 @@ export default class Main extends Component {
   };
 
   render() {
-    // eslint-disable-next-line no-unused-vars
     const { novaTarefa } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
