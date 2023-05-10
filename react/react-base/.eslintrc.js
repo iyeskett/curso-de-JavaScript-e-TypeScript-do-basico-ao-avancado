977feabd36ed2c73931b69988ccb0e8beba72a02
Felipe Mello <felipesilva.mello27@gmail.com>
2023-05-10 20:57:07 -0300

    react:EslintPrettierEEditorconfig

---
+++ react/react-base/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  settings: {},
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefere-default-export': 0,
+  },
+};
